ecc202c8b0f78f42cce3e53ed9570a7fc0641ad1
Nattawod Satee <nattawodsatee@gmail.com>
2019-04-13 14:19:06 -0700

    Setup the styling structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainContent from "./components/MainContainer";
+import "./styles/main.scss";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<MainContent />, document.getElementById("root"));
