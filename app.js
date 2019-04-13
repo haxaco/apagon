fc2c146cd36da27c443e5b85d2d54faed2ba855b
Froilan Irizarry <froilan@froilanirizarry.me>
2019-04-13 19:20:39 -0400

    Added helmet and express-rate-limit configs

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ const usersRouter = require('./routes/users');
 const app = express();
 app.use(cors());
 
+const limiter = new RateLimit({
+  windowMs: parseInt(process.env.WINDOW_MS || 60000, 10),
+  max: parseInt(process.env.MAX_IP_REQUESTS || 5000, 10),
+  delayMs:parseInt(process.env.DELAY_MS || 0, 10),
+  headers: true
+});
+app.use(limiter);
+
+app.use(function(req, res, next) {
+  res.setHeader('Server', '');
+  res.setHeader('Via', '');
+  next();
+});
+app.use(helmet.hsts({
+  maxAge: 31536000,
+  preload: false,
+  setIf: function() {
+    return process.env.NODE_ENV === 'local' ? false : true;
+  }
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
