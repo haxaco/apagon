3e6afea9d2c010db2193dfd229c9f1122b2a97ff
Harikrishnan <hkkrishn563@gmail.com>
2019-04-13 14:48:05 -0700

    react config

---
+++ client/src/components/AddressForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Forms;
