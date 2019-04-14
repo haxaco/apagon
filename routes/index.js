e31635a4f4305c792e9e82b516f9cbf27c1f5ee4
Diego Garcia <dgarcia@vlocity.com>
2019-04-14 09:03:29 -0700

    Adding DB Client

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const apagonesCollection = 'apagon';
+const lugaresCollection = 'lugares';
 const router = express.Router();
-const { getApagonesDBClient, getLugaresDBClient }= require('../lib');
+const { getApagonesDBClient }= require('../lib');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +12,7 @@ router.get('/', function(req, res, next) {
 // API routes
 router.get('/apagon', async (req, res, next) => {
   const dbClient = await getApagonesDBClient();
-  const results = await dbClient.find();
+  const results = await dbClient.find(apagonesCollection);
 
   res.json(results);
 });
@@ -20,27 +22,44 @@ router.post('/apagon', async (req, res, next) => {
   const dbClient = await getApagonesDBClient();
   const data = req.body;
   try {
-    const results = await dbClient.insert(data);
+    const results = await dbClient.insert(apagonesCollection, data);
     res.json(results);
   } catch(error) {
     console.error(error);
   }
 });
 
+
+router.get('/apagon/:id', async (req, res, next) => {
+  const dbClient = await getApagonesDBClient();
+  const results = await dbClient.find(apagonesCollection ,{
+    _id: req.params
+  });
+
+  res.json(results);
+});
+
+router.patch('/apagon/:id', async (req, res, next) => {
+  const dbClient = await getApagonesDBClient();
+  const results = await dbClient.find(lugaresCollection);
+
+  res.json(results);
+});
+
 // API routes
 router.get('/lugar', async (req, res, next) => {
-  const dbClient = await getLugaresDBClient();
-  const results = await dbClient.find();
+  const dbClient = await getApagonesDBClient();
+  const results = await dbClient.find(lugaresCollection);
 
   res.json(results);
 });
 
 
 router.post('/lugar', async (req, res, next) => {
-  const dbClient = await getLugaresDBClient();
+  const dbClient = await getApagonesDBClient();
   const data = req.body;
   try {
-    const results = await dbClient.insert(data);
+    const results = await dbClient.insert(lugaresCollection, data);
     res.json(results);
   } catch (error) {
     console.error(error);
