3e2997fe2229a8337a8afe44abf0794c4b4acc8e
Diego Garcia <dgarcia@vlocity.com>
2019-04-14 09:11:36 -0700

    Fixing Diego's mistake

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const apagonesCollection = 'apagon';
+const apagonesCollection = 'apagones';
 const lugaresCollection = 'lugares';
 const router = express.Router();
 const { getApagonesDBClient }= require('../lib');
