d09260d0a1feadec5cbbdd2223abe18acf2d1243
Harikrishnan <hkkrishn563@gmail.com>
2019-04-14 10:58:28 -0700

    User location ping

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
-
+import { GoogleComponent } from 'react-google-location' 
+const API_KEY = 'AIzaSyCvdgUHoWt0MgqmWmcw-IcMAXMvRLWxcpE'
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          place: null,
+        };
+      }
     render() {
         return (
             <div className="navigation">
                 <div className="navigation__wrapper">
                     <div className="serchbar">
-                        <form>
-                            <input type="text" className="search-input" placeholder="Serch Address"/>
-                        </form>
+                    <GoogleComponent
+                        apiKey={API_KEY}
+                        language={'en'}
+                        country={'country:in|country:us'}
+                        coordinates={true}
+                        locationBoxStyle={'custom-style'}
+                        locationListStyle={'custom-style-list'}
+                        onChange={(e) => { this.setState({ place: e }) }} />
                     </div>
                 </div>
             </div>
@@ -18,3 +30,7 @@ class Navigation extends Component {
 }
 
 export default Navigation;
+
+
+
+
