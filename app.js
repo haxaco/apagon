7ef60aaad78cbf1f797464510d4e004d7373abf3
Froilan Irizarry <froilan@froilanirizarry.me>
2019-04-14 18:58:13 -0400

    Removed view engine code

--- app.js
+++ app.js
@@ -45,17 +45,6 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up the production asset
-  // like main.js or main.css!
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // express will serve up the index.html file
-  // if it doesn't reconize the route
-  // app.get('/', (req, res) => {
-  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
-}
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -67,9 +56,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.sendStatus(err.status || 500);
 });
 
 module.exports = app;
